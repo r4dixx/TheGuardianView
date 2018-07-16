5461ad6e0dbdcabe1ec33d2828226c4bc5bf5619
r4dixx <amael.sikel@gmail.com>
2018-07-17 00:50:35 +0200

    :construction: Helper method to request/receive article data
    - New URL object from the given URL String
    - HTTP request to it returning a String as a response
    - ArrayList to parse the JSON response into objects and arrays
    - Log messages included

---
+++ app/src/main/java/com/r4dixx/theguardianview/QueryUtils.java
@@ -0,0 +1,130 @@
+package com.r4dixx.theguardianview;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class QueryUtils {
+
+    public static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    private QueryUtils() {
+    }
+
+    public static List<Article> extractFeatureFromJson(String guardianJSON) {
+
+        if (TextUtils.isEmpty(guardianJSON)) {
+            return null;
+        }
+
+        List<Article> articles = new ArrayList<>();
+
+        try {
+            JSONObject baseJsonResponse = new JSONObject(guardianJSON);
+            JSONObject responseObject = baseJsonResponse.getJSONObject("response");
+            JSONArray resultsArray = responseObject.getJSONArray("results");
+
+            for (int i = 0; i < resultsArray.length(); i++) {
+                JSONObject currentArticle = resultsArray.getJSONObject(i);
+                JSONObject fields = currentArticle.getJSONObject("fields");
+
+                String title = fields.getString("headline");
+                String url = currentArticle.getString("webUrl");
+
+                Article article = new Article(title, url);
+                articles.add(article);
+            }
+
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the Guardian JSON results", e);
+        }
+
+        return articles;
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    public static List<Article> fetchArticlesData(String requestUrl) {
+        URL url = createUrl(requestUrl);
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        List<Article> articles = extractFeatureFromJson(jsonResponse);
+        return articles;
+    }
+}
