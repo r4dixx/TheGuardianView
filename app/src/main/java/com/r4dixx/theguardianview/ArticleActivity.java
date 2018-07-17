e8ef8b20dfc06f088299dd869145a3669f62a202
r4dixx <amael.sikel@gmail.com>
2018-07-17 09:20:45 +0200

    :sparkles: :children_crossing: Swipe to refresh
    - Swipe to refresh → initLoader with refresh indicator
    - onLoadFinished → stop refreshing and hide indicator
    - empty states → stop refreshing and hide indicator

--- app/src/main/java/com/r4dixx/theguardianview/ArticleActivity.java
+++ app/src/main/java/com/r4dixx/theguardianview/ArticleActivity.java
@@ -9,6 +9,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -46,6 +47,7 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
 
         // Sets the toolbar as the app bar for the activity and hides title to allow custom text
         android.support.v7.widget.Toolbar toolBar = findViewById(R.id.toolbar);
@@ -58,7 +60,7 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
         int uiOptions = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
         decorView.setSystemUiVisibility(uiOptions);
 
-        ListView listView = findViewById(R.id.article_list);
+        ListView listView = findViewById(R.id.list);
 
         // Hook up the TextView as the empty view of the ListView
         mEmptyStateTextView = findViewById(R.id.empty_view);
@@ -81,18 +83,27 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
 
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        if (networkInfo != null && networkInfo.isConnected()) {
+        final LoaderManager loaderManager = getLoaderManager();
 
-            LoaderManager loaderManager = getLoaderManager();
+        if (networkInfo != null && networkInfo.isConnected()) {
             loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
         } else {
             View loadingIndicator = findViewById(R.id.loading_indicator);
             loadingIndicator.setVisibility(View.GONE);
             mEmptyStateTextView.setText(R.string.no_internet);
+            swipeLayout.setRefreshing(false);
         }
+
+        swipeLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        loaderManager.initLoader(ARTICLE_LOADER_ID, null, ArticleActivity.this);
+                    }
+                }
+        );
 
     }
 
@@ -108,6 +119,8 @@ public class ArticleActivity extends AppCompatActivity implements LoaderCallback
         loadingIndicator.setVisibility(View.GONE);
         mEmptyStateTextView.setText(R.string.no_articles);
         mAdapter.clear();
+        SwipeRefreshLayout swipeLayout = findViewById(R.id.swiperefresh);
+        swipeLayout.setRefreshing(false);
 
         if (earthquakes != null && !earthquakes.isEmpty()) {
             mAdapter.addAll(earthquakes);
