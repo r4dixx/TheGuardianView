aa7995cb939d6b8c670d083f55c3267317b1bc54
r4dixx <amael.sikel@gmail.com>
2018-07-19 04:29:48 +0200

    :children_crossing: Preference summary is now displayed

--- app/src/main/java/com/r4dixx/theguardianview/SettingsActivity.java
+++ app/src/main/java/com/r4dixx/theguardianview/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.r4dixx.theguardianview;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -19,11 +22,29 @@ public class SettingsActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
     }
 
-    public static class ArticlePreferenceFragment extends PreferenceFragment {
+
+    public static class ArticlePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings);
+            Preference apiKey = findPreference(getString(R.string.settings_api_key));
+            bindToValue(apiKey);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceStr = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceStr);
         }
 
     }
